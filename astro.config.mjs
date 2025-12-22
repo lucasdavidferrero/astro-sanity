3892335cd03db43275a03a0b3bcc0a642ac2cb8d
lucasdavidferrero <lucasdavidferrero@gmail.com>
2025-12-22 19:41:44 -0300

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: netlify(),
   integrations: [sanity({
     projectId: 'kqzxfqcl',
     dataset: 'production',
@@ -14,3 +16,7 @@ export default defineConfig({
     studioBasePath: '/studio'
   }), react()]
 });
+
+/* 
+https://developers.netlify.com/guides/how-to-use-sanity-cms-with-astro/#buildwebhooks
+*/
